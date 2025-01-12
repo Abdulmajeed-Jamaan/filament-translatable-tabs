d324374364e64f835ad0be67601e9b416c252b02
Abdulmajeed-Jamaan <41128358+Abdulmajeed-Jamaan@users.noreply.github.com>
2025-01-12 23:09:44 +0300

    added

---
+++ src/HasExtraConfigs.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AbdulmajeedJamaan\FilamentTranslatableTabs;
+
+use Filament\Forms\Components\Field;
+
+trait HasExtraConfigs
+{
+    public function addDirectionByLocale(): static
+    {
+        $this->modifyFieldsUsing(function (Field $component, string $locale) {
+            $dir = str($locale)->startsWith('ar') ? 'rtl' : 'ltr';
+            $component->extraAttributes(['style' => "direction: $dir;"], true);
+        });
+
+        return $this;
+    }
+
+    public function addEmptyBadgeWhenAllFieldsAreEmpty(string $emptyLable): static
+    {
+        $this->modifyTabsUsing(function (TranslatableTab $component, string $locale) use ($emptyLable) {
+            $hasValue = fn ($tab, $get): bool => collect($tab->getChildComponents())
+                ->contains(fn ($c) => ! empty($get($c->getName())));
+
+            $component
+                ->live(true)
+                ->badgeColor(fn ($component, $get) => $hasValue($component, $get) ? null : 'warning')
+                ->badge(fn ($component, $get) => $hasValue($component, $get) ? null : $emptyLable);
+        });
+
+        return $this;
+    }
+
+    public function addSetActiveTabThatHasValue(): static
+    {
+        $this->activeTab(function ($get, $component) {
+            $hasValue = function ($tab, $get): bool {
+                foreach ($tab->getChildComponents() as $component) {
+                    if (! empty($get($component->getName()))) {
+                        return true;
+                    }
+                }
+
+                return false;
+            };
+
+            $activeTabsIndex = collect($component->getChildComponents())
+                ->search(fn ($tab) => $hasValue($tab, $get));
+
+            return $activeTabsIndex === false ? 0 : $activeTabsIndex + 1;
+        });
+
+        return $this;
+    }
+}
